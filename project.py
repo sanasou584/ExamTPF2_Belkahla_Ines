1ea1a390b7dd72f4ac63dc049978bdab2a223820
benabidasma@gmail.com <benabidsana@gmail.com>
2025-03-13 10:22:41 +0100

    project.py v4

--- project.py
+++ project.py
@@ -1,4 +1,7 @@
 Data=[1,3,5]
-print('la somme est: ',sum(Data))
-print('le min est: ',min(Data))
-print('le max est: ',max(Data))
+if Data:
+    print('la somme est: ',sum(Data))
+    print('le min est: ',min(Data))
+    print('le max est: ',max(Data))
+else:
+    print('dossier vide')
